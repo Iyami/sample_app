24ad93e2e1b718493b8b8fa7dcb9ef5b37ef2f0c
Amit <amit.maheshwari04@gmail.com>
2011-03-25 14:59:09 +0530

    Done with static pages

---
+++ autotest-standalone/ruby/1.8/bin/nokogiri.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "C:/rails_projects/sample_app/autotest-standalone/ruby/1.8/bin/nokogiri" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
